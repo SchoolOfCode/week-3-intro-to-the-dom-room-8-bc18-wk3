f8bd188862d0b5a2ce51a2faf83284c775caa6ae
Nick Goh <166708778+newmaldenite@users.noreply.github.com>
2024-12-09 13:52:08 +0000

    completed tickets 1c, 1d, 2a

--- index.js
+++ index.js
@@ -29,3 +29,33 @@ console.log(document.getElementsByClassName("content"));
 - **Expected Outcome**:
   - The console should display a collection of five `<li>` elements with the content "🍎 Apple", "🍌 Banana", "🍒 Cherry", "🍇 Date", and "🍈 Fig", respectively. */
 console.log(document.getElementsByTagName("li"));
+
+// - **Instructions**:
+//   - Use `querySelector()` to select the first element with the class "highlight".
+//   - Log the selected element to the console.
+// - **Expected Outcome**:
+//   - The console should display a single `<p>` element with the content "This is another paragraph with the class 'content' 
+// and 'highlight'."
+
+console.log(document.querySelector(".highlight"))
+
+// - Use `querySelectorAll()` to select all elements with the class "highlight".
+// - Log the selected elements to the console.
+// - **Expected Outcome**:
+// - The console should display a collection of three elements: a `<p>` element with the content 
+// "This is another paragraph with the class 'content' and 'highlight'" and two `<li>` elements with the content "🍒 Cherry" and "🍈 Fig".
+
+console.log(document.querySelectorAll(".highlight"))
+
+// #### Ticket 2a: Parent Node
+
+// - **Instructions**:
+//   - Select the first `<li>` element inside the `<ul>` with the id of "itemList".
+//   - Use the `parentNode` property to access its parent.
+//   - Log the parent element to the console.
+// - **Expected Outcome**:
+//   - The console should display the entire `<ul>` element with the `id` of "itemList" and all of its `<li>` children.
+
+const listItem = (document.querySelector(".list-item"))
+console.log(listItem)
+console.log(listItem.parentNode)
