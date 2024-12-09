30e76f03d834abd2fa3a73837eed09bc9907c196
olliegwb <184382533+olliegwb@users.noreply.github.com>
2024-12-09 12:28:10 +0000

    ticket 1b and 1c

--- index.js
+++ index.js
@@ -6,3 +6,9 @@
 // The console should display the <h1> element with the content "Intro to the DOM".
 
 console.log(document.getElementById("title"));
+
+//ticket 1b
+console.log(document.getElementsByClassName("content"));
+
+//ticket 1c
+console.log(document.getElementsByTagName("li"));
