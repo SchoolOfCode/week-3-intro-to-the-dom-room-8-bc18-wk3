b1d6ca1b714bd9df2553295a44a1ee6d6556f1aa
fergus-g <fergus.gildea@utrack.com>
2024-12-09 12:11:40 +0000

    Finished ticket 1a

---
+++ index.js
@@ -0,0 +1,8 @@
+// Ticket 1a: getElementById()
+// Instructions:
+// Use getElementById() to select the element with the id of "title".
+// Log the selected element to the console.
+// Expected Outcome:
+// The console should display the <h1> element with the content "Intro to the DOM".
+
+console.log(document.getElementById("title"));
