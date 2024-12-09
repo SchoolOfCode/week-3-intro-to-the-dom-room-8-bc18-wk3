6f058d07a82b5ac7a720d98e2fa78c402d620e10
olliegwb <184382533+olliegwb@users.noreply.github.com>
2024-12-09 13:20:05 +0000

    ticket 1b and 1c

--- index.js
+++ index.js
@@ -8,7 +8,24 @@
 console.log(document.getElementById("title"));
 
 //ticket 1b
+/* Ticket 1b: `getElementsByClassName()`
+
+- **Instructions**:
+  - Use `getElementsByClassName()` to select elements with the class of "content".
+  - Log the selected elements to the console.
+- **Expected Outcome**:
+  - The console should display a collection of two `<p>` elements. The first with the content "This is a paragraph with the class 'content'." and the second with "This is another paragraph with the class 'content' and 'highlight'." */
 console.log(document.getElementsByClassName("content"));
 
+
+
+
 //ticket 1c
+/* #### Ticket 1c: `getElementsByTagName()`
+
+- **Instructions**:
+  - Use `getElementsByTagName()` to select all `<li>` elements on the page.
+  - Log the selected elements to the console.
+- **Expected Outcome**:
+  - The console should display a collection of five `<li>` elements with the content "🍎 Apple", "🍌 Banana", "🍒 Cherry", "🍇 Date", and "🍈 Fig", respectively. */
 console.log(document.getElementsByTagName("li"));
